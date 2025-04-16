75dcb1799777e044785912c521712e61a200fb43
aymanggv <62808381+aymanggv@users.noreply.github.com>
2025-04-16 15:54:03 +0400

    Add files via upload

---
+++ scripts/create_dwh_and_schemas.sql
@@ -0,0 +1,23 @@
+
+use master;
+go
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+create database DataWarehouse;
+
+use DataWarehouse;
+go 
+
+create schema bronze;
+go --go is a separator which tells sql to run first command completely before going to second command
+create schema silver;
+go
+create schema gold;
+go
